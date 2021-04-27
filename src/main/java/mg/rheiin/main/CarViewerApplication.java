d6156208bd1049f1a72bd0dadf0544a972b51e62
rheiin-n <mitchnato@gmail.com>
2021-04-27 11:10:50 +0300

    first commit

---
+++ src/main/java/mg/rheiin/main/CarViewerApplication.java
@@ -0,0 +1,13 @@
+package mg.rheiin.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarViewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mg/rheiin/main/CarViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.rheiin.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
